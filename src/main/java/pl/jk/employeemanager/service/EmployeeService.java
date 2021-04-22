40dc0ed1b5b6c441b95daf6a6053fc33acc1ee13
losu5 <losu507@tlen.pl>
2021-04-22 12:15:56 +0200

    add @transactional annotation to EmployeeService class

--- src/main/java/pl/jk/employeemanager/service/EmployeeService.java
+++ src/main/java/pl/jk/employeemanager/service/EmployeeService.java
@@ -2,6 +2,7 @@ package pl.jk.employeemanager.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import pl.jk.employeemanager.ex.UserNotFoundException;
 import pl.jk.employeemanager.model.Employee;
 import pl.jk.employeemanager.repo.EmployeeRepo;
@@ -10,6 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Service
+@Transactional
 public class EmployeeService {
     private final EmployeeRepo employeeRepo;
 
